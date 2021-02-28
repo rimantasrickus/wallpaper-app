78e057a32299f496e5c668e459add59e9abc496b
Rimantas R <rimantas.rickus@hashtages.com>
2021-02-28 20:47:33 +0200

    initial push

---
+++ src/components/Wallpaper.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          This will be wallpaper app
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+    }),
+    methods: {
+      test(url) {
+        console.log(url + ' just a test')
+      }
+    }
+  }
+</script>
